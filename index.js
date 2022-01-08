32212b5bd10c50952820d10c3a2d34b0ed51d9ec
playground <connie.s.barrantes@gmail.com>
2022-01-08 15:37:40 -0500

    created questions to prompt user

--- index.js
+++ index.js
@@ -1,7 +1,75 @@
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for information about my application repository. THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title. THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions. THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options. THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username. THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address. THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents. THEN I am taken to the corresponding section of the README
+
+
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+console.log(inquirer);
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your Github username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'input',
+        name: 'tile',
+        message: 'What is the title of your Project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of your project.'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license should your project have?',
+        choices: [
+            'Apache 2.0',
+            'BSD 3-clause',
+            'GNU v3',
+            'MIT',
+            'Mozilla Public License 2.0',
+            'Unlicensed'
+        ]
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What command should be run to install dependencies?',
+        default: 'npm i'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What command should be used to run the test?',
+        default: 'npm run test'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What would the user need to know about your repository?'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'What does the user need to know about contributing to the repository?'
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +79,15 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
